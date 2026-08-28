d1e29098f9419d1ce5aaa5c8853a4f4f8f7dfd08
Abdillah7742 <muhabdi2005@gmail.com>
2026-08-28 11:22:21 +0800

    design: terapkan flex-wrap justify-center dengan max-w untuk merata-tengahkan sisa logo skill di baris terakhir

--- script.js
+++ script.js
@@ -210,7 +210,7 @@ function populateSkills(skills) {
                     </h3>
 
                     <!-- Logos Row -->
-                    <div class="grid grid-cols-3 gap-3 justify-items-center relative z-10 w-full mt-4">
+                    <div class="flex flex-wrap justify-center gap-3 max-w-[210px] mx-auto relative z-10 mt-4">
                         ${itemsHtml}
                     </div>
                 </div>
